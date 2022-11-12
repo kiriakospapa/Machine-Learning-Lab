65fe56cb37b5766b2234f4b43d5a5d624f9b5683
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-11-12 20:01:37 +0200

    uploading the files

--- Assigment1.r
+++ Assigment1.r
@@ -3,8 +3,8 @@ library(caret)
 library(dplyr)
 library(kknn)
 library(ggplot2)
-library(plotly)
 
+# Q1
 digits = read.csv("optdigits.csv")
 set.seed(12345)
 
@@ -19,6 +19,7 @@ id = sample(1:n, floor(n*0.5))
 validation = test_and_validation[id,]
 test = test_and_validation[-id,]
 
+# Q2
 m1 = kknn(as.factor(X0.26) ~ ., train=train, test=train, k=30, kernel="rectangular")
 m2 = kknn(as.factor(X0.26) ~ ., train=train, test=test, k=30, kernel="rectangular")
 
@@ -29,9 +30,9 @@ Pred2 = m2$fitted.values
 
  
 missclass=function(X){
-  n=length(X)
-  a = 1 - sum(diag(X))
-  return(a / n)
+  n=sum(X)
+  a = sum(diag(X))/n
+  return(1-a)
 }
 
 # # It rounds up for > .5 and rounds down for <= 0.5
@@ -42,24 +43,59 @@ confusion_matrix_train =  table(train$X0.26, Pred)
 missclass(confusion_matrix_test)
 missclass(confusion_matrix_train)
 
+# Test: We can that we had many false predictions on number 5 as it misclassified 
+# as 9, 6 times. Also 9 was hard to be detected as it was misclassified as 7, 
+# 6 times. The most easy to predict was 0 as all 88 times that we had 0 it predicted
+# it correctly. In general, we can see that the missclafication error is quite low
+# so we can conclude that our model was very accurate on the test dataset
 
-min = sort(m1$prob[, 9])[1]
-#min1 = sort(m1$prob[, 9])[1]
-#min2 = sort(m1$prob[, 9])[2]
-#min3 = sort(m1$prob[, 9])[3]
+# Training: We can see that the model has predicted 1 as 8, 10 times and 9 as 5,
+# 11 times. Also our model struggled with 9 as it had many wrong predictions.
+# In addition, it was very good predicting 6 and 0. In this case too, we can see
+# that the missclassification error was very small.
+
+# Q3
+#min = sort(m1$prob[, 9])[1]
+min1 = sort(m1$prob[, 9])[1]
+min2 = sort(m1$prob[, 9])[2]
+min3 = sort(m1$prob[, 9])[3]
 
 max1 = tail(sort(m1$prob[, 9]))[1]
 max2 = tail(sort(m1$prob[, 9]))[2]
 
 # Note: Add explenation in the presentation 
-# Question: Should we take those with smallest/biggest value, or should we take unique.
+# Question: Should we take those with smallest/biggest value, or should we take unique.\\
+
+# This one is easy to recognize it visually as 8
 heatmap(t(matrix(as.numeric(train[which(m1$prob[,9] == max1)[1],1:64]), nrow=8, ncol=8)), Colv=NA,Rowv=NA)
+
+# # This one is easy to recognize it visually as 8
 heatmap(t(matrix(as.numeric(train[which(m1$prob[,9] == max2)[2],1:64]), nrow=8, ncol=8)), Colv=NA,Rowv=NA)
 
+# This one is clear that it's not 8. Visually, looks like a 0
 heatmap(t(matrix(as.numeric(train[which(m1$prob[,9] == min1)[1],1:64]), nrow=8, ncol=8)), Colv=NA,Rowv=NA)
+
+# This one is clear that it's not 8. Visually, looks like a 3
 heatmap(t(matrix(as.numeric(train[which(m1$prob[,9] == min2)[2],1:64]), nrow=8, ncol=8)), Colv=NA,Rowv=NA)
+
+#  This one is clear that it's not 8. Visually, looks like a 3
 heatmap(t(matrix(as.numeric(train[which(m1$prob[,9] == min3)[3],1:64]), nrow=8, ncol=8)), Colv=NA,Rowv=NA)
 
+
+# Q4
+# As the k increased the model gets more complicated as we have more parameters
+# to take it into account. When the k is low we can see that the error on validation
+# and training data is low. As k increases, the validation and training error increase
+# as well. The optimal k taking into account the sum of validation and training errors
+# is 2 and 3.
+
+# So for k=2 the validation error is almost equal to test error and the training
+# error is 0.
+
+# So for k=3 the the validation error is almost equal to test error and the training
+# error is  close to 0. We can conclude that the training error is smaller because
+# the model was trained on the training dataset and validation and test datasets
+# are new data and their losses are close to each other.
 
 
 validation_errors = c()
@@ -74,17 +110,45 @@ for(i in 1:30){
   validation_errors = append(validation_errors ,missclass(confusion_matrix_valid))
 }
 
+train_errors = c()
+for(i in 1:30){
+  #m1 = kknn(as.factor(X0.26) ~ ., train=train, test=train, k=i, kernel="rectangular")
+  m3 = kknn(as.factor(X0.26) ~ ., train=train, test=train, k=i, kernel="rectangular")
+  Pred3 = m3$fitted.values
+  confusion_matrix_train =  table(train$X0.26, Pred3)
+  train_errors = append(train_errors ,missclass(confusion_matrix_train))
+}
 
-error_frame = data.frame(validation_errors)
+
+error_frame = data.frame(validation_errors, train_errors, validation_errors + train_errors)
 error_frame["K"] = c(1:30)
-colnames(error_frame) = c("Error","K")
+colnames(error_frame) = c("vError", "tError", "vtError","K")
 
-p = ggplot(error_frame, aes(x=K, y=Error)) +
-  geom_line()
+p = ggplot(error_frame, aes(x=K)) +
+  geom_line(aes(y=vError), color="blue") + geom_line(aes(y=tError), color="red") +
+  geom_line(aes(y=vtError), color="green")
+p
 
-ggplotly(p)
+best_k = which(min(validation_errors + train_errors) == (validation_errors + train_errors))
+# The best ks are 1 and 2.
+
+k1 = kknn(as.factor(X0.26) ~ ., train=train, test=test, k=2, kernel="rectangular")
+k2 = kknn(as.factor(X0.26) ~ ., train=train, test=test, k=3, kernel="rectangular")
+
+Predk1 = k1$fitted.values
+confusion_matrix =  table(test$X0.26, Predk1)
+missclass(confusion_matrix)
+train_errors[2]
+validation_errors[2]
+
+Predk2 = k2$fitted.values
+confusion_matrix =  table(test$X0.26, Predk2)
+missclass(confusion_matrix)
+train_errors[3]
+validation_errors[3]
 
 
+# Q5
 # Question: Should we use base 2 or 10?
 entropy <- function(d){
   x <- 0
@@ -110,6 +174,7 @@ colnames(error_frame_2) = c("Cross_entropy","K")
 p2 = ggplot(error_frame_2, aes(x=K, y=Cross_entropy)) +
   geom_line()
 
-ggplotly(p2)
+p2
 
+# Question: hint why cross entropy might be better here.
 
--- Assigment2.r
+++ Assigment2.r
@@ -4,6 +4,7 @@ library(caret)
 # Splittimg the data
 
 data = read.csv("parkinsons.csv")
+data = subset(data, select = -c(age, sex, test_time, subject., total_UPDRS))
 
 set.seed(12345)
 
@@ -21,11 +22,10 @@ testS = predict(scaler, test_data)
 # Exercise 2
 # Creating the linear regression model
 
-fit = lm(motor_UPDRS ~ ., data = trainS)
+# For intercept = 0
+fit = lm(motor_UPDRS ~ 0 + ., data = trainS)
 
 # Calculating the MSE on training
-# Intercept is very low(1.534e-15) So i can consider that it's zero
-
 #model_summ <-summary(fit)
 #mse_training = mean(model_summ$residuals^2) # That's a second way
 results = predict(fit, trainS)
@@ -39,12 +39,100 @@ mse_test = (1/n) * sum((results - testS$motor_UPDRS)^2)
 
 # Finding the most significant variables
 # Visualize the ones with the biggest values in t and visualize it(Damian's task)
+#is a univariate real-valued function, i.e., f : R 
 summary(fit)
 
 
 # Exercise 3
 # Log Likehood function
-
-loglikehood <- function(theta, sigma){
-  
+loglikelihood <- function(params, data){
+  print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+  sigma <- params[1]
+  print(length(params))
+  w <- params[2:ncol(data)]
+  y <- data[,1]
+  x <- data[, 2:ncol(data)]
+  n <- nrow(x)
+  loglikelihood <- -n/2 * log(2*pi*sigma^2) - 1/(2*sigma^2) * sum((y - as.matrix(x) %*% as.numeric(w))^2) 
+  return(loglikelihood)
 }
+
+ridge <- function(params, data, lambda){
+  w <- params[2:length(params)]
+  loglikelihood <- -loglikelihood(params, data) + lambda*sum(w^2)
+  return(loglikelihood)
+}
+
+ridgeOpt <- function(lambda, data){
+  opt <- optim(fit$coefficients, fn=ridge, data=data, lambda=lambda, method="BFGS")
+  return(opt)
+}
+
+df <- function(lambda, x){
+  x<- as.matrix(x)
+  out <- x %*% solve((t(x) %*% x) + diag(lambda, ncol(x))) %*% t(x)
+  out <- sum(diag(out))
+  return(out)
+}
+
+trainS_hat = trainS["motor_UPDRS"]
+#trainS = subset(trainS, select = -c(motor_UPDRS))
+
+testS_hat = testS["motor_UPDRS"]
+#testS = subset(testS, select = -c(motor_UPDRS))
+
+
+model1 = ridgeOpt(1, trainS)
+model2 = ridgeOpt(100, trainS)
+model3 = ridgeOpt(1000, trainS)
+
+# p 17 s(x) * p is p. we have to find df by it's trace
+# https://stats.stackexchange.com/questions/8309/how-to-calculate-regularization-parameter-in-ridge-regression-given-degrees-of-f
+
+opt_predict <- function(x, model){
+  out <- sum((x) * model$par)
+  print(out)
+  return(out)
+}
+
+train_output1 <- c()
+train_output2 <- c()
+train_output3 <- c()
+
+for(i in 1:nrow(trainS)){
+  
+  train_output1[i] <- opt_predict(trainS[i, ], model1)
+  train_output2[i] <- opt_predict(trainS[i, ], model2)
+  train_output3[i] <- opt_predict(trainS[i, ], model3)
+}
+
+test_output1 <- c()
+test_output2 <- c()
+test_output3 <- c()
+
+for(i in 1:nrow(testS)){
+  test_output1[i] <- opt_predict1(testS[i, ], model1)
+  test_output2[i] <- opt_predict1(testS[i, ], model2)
+  test_output3[i] <- opt_predict1(testS[i, ], model3)
+}
+
+ms_train1 <- (1/nrow(trainS_hat)) * (sum(train_output1 - trainS_hat)) ^ 2
+ms_train2 <- (1/nrow(trainS_hat)) * (sum(train_output2 - trainS_hat)) ^ 2
+ms_train3 <- (1/nrow(trainS_hat)) * (sum(train_output3 - trainS_hat)) ^ 2
+
+
+ms_test1 <- (1/nrow(testS_hat)) * (sum(test_output1 - testS_hat))^ 2
+ms_test2 <- (1/nrow(testS_hat)) * (sum(test_output2 - testS_hat)) ^ 2
+ms_test3 <- (1/nrow(testS_hat)) * (sum(test_output3 - testS_hat)) ^ 2
+
+df(1, trainS)
+df(100, trainS)
+df(1000, trainS)
+
+df(1, testS)
+df(100, testS)
+df(1000, testS)
+
+# T value of a coefficient is the estimate devided by st error of the coefficent.
+# As lower as p is as more statistically significant relationship with final exam score is
+# If standard error is much smaller than the expected value then the p value will be sifniciant
