584dffc1e165e708400af1309506c9a2b7b8075a
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-11-22 10:58:59 +0200

    Finished 4 out of 5

--- LAB2/Assigment 2.R
+++ LAB2/Assigment 2.R
@@ -41,38 +41,65 @@ mse_test = (1/n) * sum((results_test - test$Fat) ^ 2)
 # Question 2: According to LASSO we should scale our data, but exercise doesn't mention
 # something like that
 
+# Just write the formula of lasso regression
+
 
 ## 3.
 
 # Question 1: Should we do it like that or use the optim function?
 # Does it mean to show the degrees of freedom depending on lambda?
 
+# Not sure if we need scaling
+
 # The first column in the number of samples
-scaler = preProcess(train)
-trainS = predict(scaler, train)
-testS = predict(scaler, test)
+# scaler = preProcess(train)
+# trainS = predict(scaler, train)
+# testS = predict(scaler, test)
 
 
 
-covariates = trainS[, !names(trainS) %in% c("Fat")]
-response = trainS[, names(trainS) %in% c("Fat")]
+covariates = train[, !names(train) %in% c("Fat", "Protein", "Moisture")]
+response = train[, names(train) %in% c("Fat")]
 
-model0 = glmnet(as.matrix(covariates), response, alpha = 1, family="gaussian")
+modell = glmnet(as.matrix(covariates), response, alpha = 1, family="gaussian",
+                lambda = seq(0, 1, 0.001))
+plot(modell, xvar="lambda", label=TRUE)
 
-
-lasso_function <- function(params, data, lambda){
-  n = length(params)
-  intercept = params[1]
-  thetas = params[2: n]
-  x = data[ , !names(trainS) %in% c("Fat")]
-  y = data[ , names(trainS) %in% c("Fat")]
-
-  out <- (1 / n) * sum((y - intercept -(thetas) * x) ^ 2)  + lambda * sum(abs(thetas)) 
-  return(out)
+values_of_lambda = c()
+for(i in 1:length(modell$df)){
+  if(modell$df[i] == 3){
+    values_of_lambda <- append(values_of_lambda, modell$lambda[i])
+  }
 }
 
+# The values of lambda that we hade df =  3
+from = min(values_of_lambda)
+to = max(values_of_lambda)
 
-optim(rep(100, ncol(data)), fn = lasso_function, data = trainS, lambda = 100)
+## 4.
+
+modelr = glmnet(as.matrix(covariates), response, alpha = 0, family="gaussian",
+                lambda = seq(0, 1, 0.001))
+
+plot(modelr, xvar="lambda", label=TRUE)
+
+# We can say that lasso gets rid of more features than ridge.
+
+modelcv=cv.glmnet(as.matrix(covariates), response, alpha=1, family="gaussian")
+optimal_lambda = modelcv$lambda.min
+plot(modelcv)
+coef(modelcv, s="lambda.min")
+cv_scores = modelcv$cvm
+lambdas = modelcv$lambda
+
+
+# This part of the code is not necessary as we do it with plot(modelcv)
+# df = data.frame(cv_scores, lambdas)
+# p = ggplot(data = df, aes(x=log(lambdas, base = 10), y=cv_scores)) +
+#   geom_line()
+# p
+# For the annyoing error
+# par(mar=c(1,1,1,1))
 
 
 
